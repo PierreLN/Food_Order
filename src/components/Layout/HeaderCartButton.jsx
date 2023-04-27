ec81fb3fab0615b3f8396a716ba87fd1f27f38fd
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-04-27 14:50:21 -0400

    ordering render modal

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,11 +1,28 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import Modal from "../UI/Modal";
 
 const HeaderCartButton = (props) => {
+  const [added, setAdded] = useState(false);
+
+  const orderingHandler = (props) => {
+    setAdded(true);
+  };
+
+  const modalOff = () => {
+    setAdded(false);
+  };
   return (
     <Fragment>
-      <button className={styles.button}>
+      {added && (
+        <Modal
+          title="Ordering ..."
+          message="incomming..."
+          onConfirm={modalOff}
+        ></Modal>
+      )}
+      <button className={styles.button} onClick={orderingHandler}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,11 +1,9 @@
-import React, { Fragment , useState } from "react";
+import React, { Fragment, useState } from "react";
 import styles from "./MealItemForm.module.css";
 import Input from "../../UI/Input.jsx";
-import Modal from "../../UI/Modal";
 
 const MealItemForm = (props) => {
   const [amount, setAmount] = useState(1);
-  const [added, setAdded] = useState(false);
 
   const enteredData = (event) => {
     setAmount(event.target.value);
@@ -16,39 +14,27 @@ const MealItemForm = (props) => {
     event.preventDefault();
     props.amountAdded(amount);
     setAmount(1);
-    setAdded(true);
   };
 
-  const modalOff = () => {
-    setAdded(false);
-  }
-
-  return ( <Fragment>
-    {added && (
-      <Modal
-      title='Ordering ...'
-      message='incommig...'
-      onConfirm={modalOff}>
-      </Modal>
-    )}
-    <form className={styles.form} onSubmit={submitHandler}>
-      <Input
-        label="Amount"
-        input={{
-          id: 'Amount_' + props.id,
-          type: "number",
-          min: "1",
-          max: "5",
-          step: "1",
-          defaultValue: '1',
-          value: amount,
-          onChange: enteredData,
-        }}
-      ></Input>
-      <button type="submit">+Add</button>
-    </form>
-  </Fragment>
-
+  return (
+    <Fragment>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <Input
+          label="Amount"
+          input={{
+            id: "Amount_" + props.id,
+            type: "number",
+            min: "1",
+            max: "5",
+            step: "1",
+            defaultValue: "1",
+            value: amount,
+            onChange: enteredData,
+          }}
+        ></Input>
+        <button type="submit">+Add</button>
+      </form>
+    </Fragment>
   );
 };
 
