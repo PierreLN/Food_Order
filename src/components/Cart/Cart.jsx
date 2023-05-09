501e8c3cc2ab973ef335a2302ae4b79ce2fcbc80
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-05-09 14:34:44 -0400

    total up

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,21 +7,29 @@ const Cart = (props) => {
     console.log(props.addOrderedItem);
   };
 
-  const cartList = <ul className={styles["cart-items"]}>
-    {(props.addOrderedItem).map((item) => (
-      <div className={styles.items}>
-        <div>{item[0].name}</div>
-        <div>{`${item[0].price}$`}</div>
-      </div>
-    ))}
-  </ul>
+  let totalPrice = 0;
+
+  const cartList = (
+    <ul className={styles["cart-items"]}>
+      {props.addOrderedItem.map((item) => {
+        totalPrice += item[0].price * item[0].amount;
+        return (
+          <div className={styles.items}>
+            <div>{`${item[0].amount} x ${item[0].name}`}</div>
+            <div>{`${item[0].price}$`}</div>
+            <div>{`${item[0].amount * item[0].price}$`}</div>
+          </div>
+        );
+      })}
+    </ul>
+  );
 
   return (
     <Modal onConfirm={props.onConfirm}>
       {cartList}
       <div className={styles.total}>
-        <div onClick={orderHandler}>Total Amount</div>
-        <div>{`${"32"} $`}</div>
+        <div>Total Amount</div>
+        <div>{`${(totalPrice).toFixed(2)} $`}</div>
       </div>
       <div className={styles.actions}>
         <button className={styles["button--alt"]} onClick={props.onConfirm}>
